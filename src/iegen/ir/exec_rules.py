3d392cd0d4fd676e027e5d44bdca9151b80f0bc7
liana.karapetyan <lianak0506@gmail.com>
2021-02-01 17:34:02 +0400

    IEG-53 Removed redundant code.

--- src/iegen/ir/exec_rules.py
+++ src/iegen/ir/exec_rules.py
@@ -142,8 +142,7 @@ class Context(object):
 
         def walk(base_types):
             for base in base_types:
-                base = self.find_by_type(cutil.template_type_name(base)) or \
-                       self.find_by_type(cutil.template_type_name(base.get_canonical()))
+                base = self.find_by_type(base)
                 yield base
                 for base in walk(base.base_types):
                     yield base

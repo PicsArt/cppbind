ce1b1dfea69016f26304832c958ec4ca36492e61
liana.karapetyan <lianak0506@gmail.com>
2021-05-12 14:21:36 +0400

    IEG-125 - Removed redundant code.

--- src/iegen/converter/python.py
+++ src/iegen/converter/python.py
@@ -104,14 +104,3 @@ def get_default_value(arg):
             return f' = {arg.converter.python.target_type_name}.{arg.default}'
         return f' = {arg.default}'
     return ''
-
-
-def get_pybind_default_value(arg):
-    if arg.default:
-        pointee = cutil.get_pointee_type(arg.cursor.type)
-        if arg.default in ('nullptr', 'NULL'):
-            return f' = static_cast<{arg.converter.python.cxx_type_name}>({arg.default})'
-        elif pointee.kind == cli.TypeKind.ENUM:
-            return f' = {pointee.spelling}::{arg.default}'
-        return f' = {arg.default}'
-    return ''

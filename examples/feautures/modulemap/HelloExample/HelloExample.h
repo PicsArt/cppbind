5d6a25e91fa14145cb445d5f7824c8e2c79653ab
liana.karapetyan <lianak0506@gmail.com>
2021-10-29 18:11:51 +0400

    I286 - Fixed headers, fixed helper includes, added new variables for modulemap.

---
+++ examples/feautures/modulemap/HelloExample/HelloExample.h
@@ -0,0 +1,16 @@
+//
+//  HelloExample.h
+//  HelloExample
+//
+//  Created by Liana on 26.10.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloExample.
+FOUNDATION_EXPORT double HelloExampleVersionNumber;
+
+//! Project version string for HelloExample.
+FOUNDATION_EXPORT const unsigned char HelloExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloExample/PublicHeader.h>

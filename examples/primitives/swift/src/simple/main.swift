2d4ed97dd3cf7a1e9e267d6dbd6934ffd1d98a75
Liana Karapetyan <lianak0506@gmail.com>
2021-12-02 10:55:06 +0400

    I337 - Updated swift usage tests to be run on macos and linux.

--- examples/primitives/swift/src/simple/main.swift
+++ examples/primitives/swift/src/simple/main.swift
@@ -1,29 +1,43 @@
+import XCTest
 import Wrapper
 
-// [task-usage]
-let task = Task(title: "My Task")
-assert(task.title == "My Task")
-// [task-usage]
+func runSimpleExamples() {
+    // [task-usage]
+    let task = Task(title: "My Task")
+    assert(task.title == "My Task")
+    // [task-usage]
 
-// [project-usage]
-let title = "My Project"
-let prj = Project(title: title)
-assert(prj.title == title)
+    // [project-usage]
+    let title = "My Project"
+    let prj = Project(title: title)
+    assert(prj.title == title)
 
-let task1 = Task(title: "My Task")
-prj.addTask(task: task1)
-assert(prj.tasks().count == 1)
-assert(prj.tasks()[0].title == task1.title)
-// [project-usage]
+    let task1 = Task(title: "My Task")
+    prj.addTask(task: task1)
+    assert(prj.tasks().count == 1)
+    assert(prj.tasks()[0].title == task1.title)
+    // [project-usage]
 
-// [root-usage]
-let path = "/path/to/root"
-let root = Root(_path: path)
-assert(root.path == path)
-// [root-usage]
+    // [root-usage]
+    let path = "/path/to/root"
+    let root = Root(_path: path)
+    assert(root.path == path)
+    // [root-usage]
 
-// test for checking how const& return type is handled by pybind
-let holder = Holder()
-print(holder.task.title)
-holder.task.setTitle(title: "My First Task Edited")
-assert(holder.task.title == "My First Task Edited")
+    // test for checking how const& return type is handled by pybind
+    let holder = Holder()
+    print(holder.task.title)
+    holder.task.setTitle(title: "My First Task Edited")
+    assert(holder.task.title == "My First Task Edited")
+}
+
+#if os(Linux)
+runSimpleExamples()
+#elseif os(OSX)
+class SimpleTests: XCTestCase {
+
+    func testRun() throws {
+        runSimpleExamples()
+    }
+}
+#endif

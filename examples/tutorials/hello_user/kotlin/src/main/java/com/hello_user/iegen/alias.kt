e3252f3c40c4c50e1d428fc46e7a44733b0c2eeb
Movses Vardanyan <movses.vardanyan@picsart.com>
2022-02-15 16:57:08 +0400

    Changes of hello_user example after run

--- examples/tutorials/hello_user/kotlin/src/main/java/com/hello_user/iegen/alias.kt
+++ examples/tutorials/hello_user/kotlin/src/main/java/com/hello_user/iegen/alias.kt
@@ -13,3 +13,5 @@ package com.hello_user.iegen.alias
 
 typealias StringArray = Array<String>
 typealias ObjectArray = Array<Any>
+
+data class IEGenObject(val id : Long, val owner : Boolean = false)

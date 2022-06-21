895a7a7bd29627b57225694956dd2463226da41f
Davit Hambardzumyan <davit.hambardzumyan@picsart.com>
2022-06-21 06:27:55 +0000

    IEGEN-447: Added CI bazel tests for tutorials

--- examples/tutorials/array/usage/swift/array_usage.swift
+++ examples/tutorials/array/usage/swift/array_usage.swift
@@ -1,31 +1,38 @@
+import XCTest
 import Wrapper
 
-@main
-class ArrayTest {
+func runArrayTests() {
 
-    static func main() {
+    let array = Array()
 
-        let array = Array()
+    assert(array.empty)
 
-        assert(array.empty)
+    array.push_back(element: 0)
+    array.push_back(element: 1)
+    array.push_back(element: 2)
 
-        array.push_back(element: 0)
-        array.push_back(element: 1)
-        array.push_back(element: 2)
+    assert(!array.empty)
+    assert(array.size == 3)
+    assert(array.front() == 0)
+    assert(array.back() == 2)
+    assert(array[1] == 1)
 
-        assert(!array.empty)
-        assert(array.size == 3)
-        assert(array.front() == 0)
-        assert(array.back() == 2)
-        assert(array[1] == 1)
+    do {
+        let _ = try array.at(i: 5)
+        assert(false)
+    } catch is StdOutOfRange {
+    } catch {
+        assert(false)
+    }
 
-        do {
-            let _ = try array.at(i: 5)
-            assert(false)
-        } catch is StdOutOfRange {
-        } catch {
-            assert(false)
-        }
+}
 
+#if os(Linux)
+runArrayTests()
+#elseif os(OSX)
+class ArrayTests: XCTestCase {
+    func testRun() throws {
+        runArrayTests()
     }
 }
+#endif
--- examples/tutorials/hello_user/usage/swift/hello_user_usage.swift
+++ examples/tutorials/hello_user/usage/swift/hello_user_usage.swift
@@ -1,25 +1,33 @@
+import XCTest
 import Wrapper
 
-@main
-class HelloUser {
+func runHelloUserTests() {
 
-    static func main() {
+    let user = UserInfo(userName: "John", userAge: 22)
+    let young_user = UserInfo(userName: "Kate", userAge: 18)
 
-        let user = UserInfo(userName: "John", userAge: 22)
-        let young_user = UserInfo(userName: "Kate", userAge: 18)
+    let host = Wrapper.Host()
 
-        let host = Host()
+    assert(host.hello(user: user) == "Hello John")
+    assert(host.hello(user: young_user) == "Hi Kate")
 
-        assert(host.hello(user: user) == "Hello John")
-        assert(host.hello(user: young_user) == "Hi Kate")
+    assert(host.welcome(user: user) == "Welcome John! Let me know if you want something.")
+    assert(host.welcome(user: young_user) == "Welcome Kate! Let me know if you want something.")
 
-        assert(host.welcome(user: user) == "Welcome John! Let me know if you want something.")
-        assert(host.welcome(user: young_user) == "Welcome Kate! Let me know if you want something.")
+    user.want_a_drink = true
+    young_user.want_a_drink = true
 
-        user.want_a_drink = true
-        young_user.want_a_drink = true
+    assert(host.welcome(user: user) == "Welcome John! Do you want cap of beer?")
+    assert(host.welcome(user: young_user) == "Welcome Kate! Do you want cap of juice?")
 
-        assert(host.welcome(user: user) == "Welcome John! Do you want cap of beer?")
-        assert(host.welcome(user: young_user) == "Welcome Kate! Do you want cap of juice?")
+}
+
+#if os(Linux)
+runHelloUserTests()
+#elseif os(OSX)
+class HelloUserTests: XCTestCase {
+    func testRun() throws {
+        runHelloUserTests()
     }
 }
+#endif
